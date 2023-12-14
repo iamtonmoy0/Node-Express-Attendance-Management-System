bb06429218f6dea757c68afbe84797c858c7b9a7
iamtonmoy0 <tonmoy2code@gmail.com>
2023-12-14 18:25:43 +0600

    initial server started

---
+++ server.js
@@ -0,0 +1,12 @@
+const http = require("http");
+const app = require("./app/app");
+require("colors");
+
+// port
+const port = process.env.PORT || 5000;
+// initialize server
+const server = http.createServer(app);
+
+server.listen(port, () => {
+  console.log(`server has started and listening on port ${port}`.green.bold);
+});
