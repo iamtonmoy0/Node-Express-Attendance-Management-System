9814129a0727127acc9fccaa3221b2cd0ae298d1
iamtonmoy0 <tonmoy2code@gmail.com>
2023-12-18 00:56:21 +0600

    profile schema added

--- models/profile.model.js
+++ models/profile.model.js
@@ -1,3 +1,16 @@
 const { Schema, model } = require("mongoose");
 
 const { ObjectId } = Schema;
+
+const profileSchema = new Schema({
+  firstName: String,
+  lastName: String,
+  phone: String,
+  avatar: String,
+  user: {
+    type: ObjectId,
+    ref: "User",
+  },
+});
+
+const profile = model("Profile", profileSchema);
