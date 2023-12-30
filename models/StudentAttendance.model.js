f816e4ff4a7b82e1716779d137f0b6f6726d4237
iamtonmoy0 <tonmoy2code@gmail.com>
2023-12-30 19:19:45 +0600

    attendance model added

--- models/StudentAttendance.model.js
+++ models/StudentAttendance.model.js
@@ -1,3 +1,18 @@
 const { Schema, model } = require("mongoose");
-
 const { ObjectId } = Schema;
+
+const studentAttendanceSchema = new Schema({
+  createdAt: Date,
+  user: {
+    type: ObjectId,
+    ref: "User",
+  },
+  adminAttendance: {
+    type: ObjectId,
+    ref: "AdminAttendance",
+  },
+});
+
+const StudentAttendance = model("StudentAttendance", studentAttendanceSchema);
+
+module.exports = StudentAttendance;
